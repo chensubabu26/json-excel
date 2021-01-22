3950ae65aed300baa77c00769fdeaf9f91b7ce87
chensubabu26 <chensu.babu@gmail.com>
2021-01-22 10:56:32 +0530

    changed json properties and methods

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# json-excel
+# exporting json to excel
+
+# input-file : sample.json
+# output-file : output.xlsx
--- convert.js
+++ convert.js
@@ -1,21 +1,42 @@
+//importing sample.json
 const allUsers = require('./sample.json');
+//import fs -> path, express -> server, jsonexport -> exporter, csvConverter.
 const fs = require('fs');
 const app = require('express')();
-var json2xls = require('json2xls');
-const filename = 'sample.xlsx';
-app.listen(5050, () => {
-  console.log("app is running on port 5050");
-  convert();
+var jsonexport = require('jsonexport');
+const convertCsvToXlsx = require('@aternus/csv-to-xlsx');
+const path = require('path');
+const filename = 'sample.csv';
+
+app.get('/',(req,res) => {
+  res.send('Export of json to excel is done... pls check the file directory.');
 })
+
 var convert = function () {
-  var xls = json2xls(allUsers);
-  fs.writeFileSync(filename, xls, 'binary', (err) => {
-     if (err) {
-           console.log("writeFileSync :", err);
-      }
-    console.log( filename+" file is saved!");
+  jsonexport(allUsers,(err,csv) => {
+    fs.writeFileSync(filename, csv, 'binary', (err) => {
+       if (err) {
+             console.log("writeFileSync :", err);
+        }
+      console.log( filename+" file is saved!");
+   });
  });
+
+ let source = path.join(__dirname, 'sample.csv');
+ let destination = path.join(__dirname, 'output.xlsx');
+  try {
+    convertCsvToXlsx(source, destination);
+    fs.unlink('sample.csv',(err) => {
+      if(err){
+        console.log(err);
+      }
+    })
+  } catch (e) {
+    console.log(e.toString());
+  }
 }
 
-
-// npm install, npm install express, npm install json2xls
+app.listen(3001, () => {
+  console.log("app is running on port 3001");
+  convert();
+})
